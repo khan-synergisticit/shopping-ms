8646d3dd3377f4d191e02481ec8c2be472850628
donkey <khansession55@gmail.com>
2024-11-04 01:26:09 +0000

    url

--- src/main/java/com/americanworx/shoppingms/config/Constants.java
+++ src/main/java/com/americanworx/shoppingms/config/Constants.java
@@ -1,6 +1,6 @@
 package com.americanworx.shoppingms.config;
 
 public class Constants {
-    public static final String JWT_AUTH_SERVER="http://192.168.1.75:8040";
-    public static final String FRONTEND_URL="http://192.168.1.69:8080";
+    public static final String JWT_AUTH_SERVER="http://192.168.1.129:8040";
+    public static final String FRONTEND_URL="http://192.168.1.111:8080";
 }
