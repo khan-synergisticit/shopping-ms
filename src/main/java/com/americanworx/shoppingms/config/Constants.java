759cfa387613c14c26483cffe8619fef764c3dd3
american-worx <khan@americanworx.com>
2024-11-15 12:24:03 -0500

    url

--- src/main/java/com/americanworx/shoppingms/config/Constants.java
+++ src/main/java/com/americanworx/shoppingms/config/Constants.java
@@ -2,5 +2,5 @@ package com.americanworx.shoppingms.config;
 
 public class Constants {
     public static final String JWT_AUTH_SERVER="http://10.96.0.11:8040";
-    public static final String FRONTEND_URL="http://192.168.1.69:8080";
+    public static final String FRONTEND_URL="http://192.168.61.11:8080";
 }
